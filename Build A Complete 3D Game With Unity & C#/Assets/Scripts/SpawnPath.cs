c04372d92a30eb5c7fee8fbdaf4f6690757970e0
folktanakorn <tanakorn.j@cdl.co.th>
2023-06-20 18:54:59 +0700

    Random Spawner

--- Build A Complete 3D Game With Unity & C#/Assets/Scripts/SpawnPath.cs
+++ Build A Complete 3D Game With Unity & C#/Assets/Scripts/SpawnPath.cs
@@ -7,23 +7,42 @@ public class SpawnPath : MonoBehaviour
     public GameObject PathPrefabs;
     Vector3 lastPosition;
     float size;
+    public bool GameOver;
     // Start is called before the first frame update
     void Start()
     {
         lastPosition = PathPrefabs.transform.position;
         size = PathPrefabs.transform.localScale.x;
-        for (int i = 0; i < 5 ;i++)
+        for (int i = 0; i < 20; i++)
         {
-            SpawnZ();
+            SpawnRandom();
         }
 
+        InvokeRepeating("SpawnRandom",2f,0.2f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (GameOver)
+        {
+            CancelInvoke("SpawnRandom");
+        }
     }
+
+    void SpawnRandom()
+    {
+        int RandomNumber = Random.Range(0, 6);
+        if (RandomNumber > 3)
+        {
+            SpawnX();
+        }
+        else if(RandomNumber <= 3){
+            SpawnZ();
+        }
+    }
+
+     
     void SpawnX()
     {
         Vector3 pos = lastPosition;
