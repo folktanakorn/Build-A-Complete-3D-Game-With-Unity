948a44a5eea4903a9e9ffb68c525c94943dbbab3
folktanakorn <tanakorn.j@cdl.co.th>
2023-06-20 15:33:22 +0700

    Make Platforms Fall Down After Ball Goes Away

---
+++ Build A Complete 3D Game With Unity & C#/Assets/Scripts/TriggerCheck.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerCheck : MonoBehaviour
+{
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Ball")
+        {
+            Debug.Log("Ball Enter");
+            Invoke("FallDown",0.2f);
+        }
+    }
+
+    void FallDown()
+    {
+        GetComponentInParent<Rigidbody>().useGravity = true;
+
+        Destroy(transform.parent.gameObject,2f);
+
+    }
+
+}
