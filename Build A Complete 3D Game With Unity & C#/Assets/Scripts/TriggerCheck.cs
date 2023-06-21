c1eb5eeb9188481d30be5c4e33117776d0455f11
folktanakorn <tanakorn.j@cdl.co.th>
2023-06-21 09:36:49 +0700

    Fixing Platform Falling

--- Build A Complete 3D Game With Unity & C#/Assets/Scripts/TriggerCheck.cs
+++ Build A Complete 3D Game With Unity & C#/Assets/Scripts/TriggerCheck.cs
@@ -16,7 +16,7 @@ public class TriggerCheck : MonoBehaviour
     void FallDown()
     {
         GetComponentInParent<Rigidbody>().useGravity = true;
-
+        GetComponentInParent<Rigidbody>().isKinematic = false;
         Destroy(transform.parent.gameObject,2f);
 
     }
